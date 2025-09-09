cda2a44b14023c99212eec27a1ca12095e9bd9d6
malcom-mo <m@mal.com.de>
2025-09-09 10:19:51 +0200

    Simplify examples

--- starter-pack/themes/myDarkTheme/myDarkTheme-per-shell.sh
+++ starter-pack/themes/myDarkTheme/myDarkTheme-per-shell.sh
@@ -8,15 +8,6 @@ export TMUX_THEME=~/.config/skinner/themes/myDarkTheme/dark-tmux.conf
 
 # bat
 export BAT_THEME=Coldark-Dark
-# alias bat="bat --theme Coldark-Dark"
-
-# bat as man pager
-export MANPAGER="sh -c 'col -bx | bat --language man --style plain --theme Coldark-Dark'"
-
-# bat as git pager
-export GIT_CONFIG_COUNT=1
-export GIT_CONFIG_KEY_0="core.pager"
-export GIT_CONFIG_VALUE_0="bat --style=plain --theme=Coldark-Dark"
 
 # fzf
 export FZF_DEFAULT_OPTS="--color dark"
--- starter-pack/themes/myLightTheme/myLightTheme-per-shell.sh
+++ starter-pack/themes/myLightTheme/myLightTheme-per-shell.sh
@@ -8,15 +8,6 @@ export TMUX_THEME=~/.config/skinner/themes/myLightTheme/light-tmux.conf
 
 # bat
 export BAT_THEME=Coldark-Cold
-# alias bat="bat --theme Coldark-Cold"
-
-# bat as man pager
-export MANPAGER="sh -c 'col -bx | bat --language man --style plain --theme Coldark-Cold'"
-
-# bat as git pager
-export GIT_CONFIG_COUNT=1
-export GIT_CONFIG_KEY_0="core.pager"
-export GIT_CONFIG_VALUE_0="bat --style=plain --theme=Coldark-Cold"
 
 # fzf
 export FZF_DEFAULT_OPTS="--color light"
